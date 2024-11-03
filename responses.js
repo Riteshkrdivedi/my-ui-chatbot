ad6d3c405165da91dfb2a79f1e15bbaf370456ce
Ritesh <riteshkd2k20@gmail.com>
2024-11-03 16:25:01 +0530

    ui

---
+++ responses.js
@@ -0,0 +1,22 @@
+export const responses = {
+  "How can we know if God exists? Can a Creator really be proven?": {
+    text: "There are various philosophical arguments that aim to prove the existence of a Creator...",
+    audioUrl: "./ans-1.mp3",
+  },
+  "What is man? Is man an animal?": {
+    text: "Man is indeed a unique creation with both physical and spiritual aspects...",
+    audioUrl: "path/to/audio2.mp3",
+  },
+  "Do Bahá’ís believe in the evolution of species?": {
+    text: "Bahá’ís acknowledge both the divine creation and the evolutionary process...",
+    audioUrl: "path/to/audio3.mp3",
+  },
+  "How did humans come to be?": {
+    text: "Human existence is believed to be the result of divine creation as well as evolution...",
+    audioUrl: "path/to/audio4.mp3",
+  },
+  "Who is Bahá’u’lláh? What can you tell me about Bahá’u’lláh?": {
+    text: "Bahá’u’lláh is the founder of the Bahá’í Faith, revered as a prophet...",
+    audioUrl: "path/to/audio5.mp3",
+  },
+};
